0e135aa2f06d8fe624f7ecee23cd502f3e6a49ec
Aaav <rakesh.g2709.com>
2024-01-08 22:24:00 +0530

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ImageGenerator from './Components/ImageGenerator';
+
+function App() {
+  return (
+    <>
+    <h1>Generate Any Image From Text Entered</h1>
+    <div className="App">
+      
+      <ImageGenerator />
+    </div>
+    </>
+  );
+}
+
+export default App;
